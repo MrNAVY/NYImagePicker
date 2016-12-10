b05d34d23857cad9bc4e8a033f543e1841c4293d
MrNAVY <navydun@163.com>
2016-12-10 10:48:14 +0800

    init demo

---
+++ NYImagePicker/NYImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYImagePicker
+//
+//  Created by navy deng on 2016/12/10.
+//  Copyright © 2016年 NY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
